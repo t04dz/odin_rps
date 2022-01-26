2539e381863696683bc3b5b916c1da5702d2670a
t04dz <powindahh4@gmail.com>
2022-01-25 20:17:54 -0700

    added win state, formatted code nicely

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 let cpuSelectText = document.getElementById("cpuSelectText")
 let wolText = document.getElementById("winOrLose")
 
-function cpuPlay(){
-    let cpuChoices = ["Rock", "Paper", "Scissors"]
-    return cpuChoices [Math.floor(Math.random() * cpuChoices.length)]
-    
+function cpuPlay() {
+  let cpuChoices = ["Rock", "Paper", "Scissors"]
+  return cpuChoices[Math.floor(Math.random() * cpuChoices.length)]
 }
-function playRound(playerSelect){
-    let cpuSelect = cpuPlay()
-        if ((playerSelect == "Rock" && cpuSelect == "Paper") ||
-            (playerSelect == "Paper" && cpuSelect == "Scissors") ||
-            (playerSelect == "Scissors" && cpuSelect == "Rock")){
-                wolText.textContent = "You lose!"
-            } else if (playerSelect == cpuSelect){
-                wolText.textContent = "You tie!"
-            }
-    cpuSelectText.textContent = "Computer picks: " + cpuSelect 
-    
-    
+function playRound(playerSelect) {
+  let cpuSelect = cpuPlay()
+  if (
+    (playerSelect == "Rock" && cpuSelect == "Paper") ||
+    (playerSelect == "Paper" && cpuSelect == "Scissors") ||
+    (playerSelect == "Scissors" && cpuSelect == "Rock")
+  ) {
+    wolText.textContent = "You lose!"
+  } else if (playerSelect == cpuSelect) {
+    wolText.textContent = "You tie!";
+  } else {
+    wolText.textContent = "You win!"
+  }
+  cpuSelectText.textContent = "Computer picks: " + cpuSelect
 }
 
-
-
-document.querySelectorAll('input').forEach(button =>{
-    button.addEventListener('click', function(){
-        playRound(button.value)
-    })
+document.querySelectorAll("input").forEach((button) => {
+  button.addEventListener("click", function () {
+    playRound(button.value)
+  })
 })
